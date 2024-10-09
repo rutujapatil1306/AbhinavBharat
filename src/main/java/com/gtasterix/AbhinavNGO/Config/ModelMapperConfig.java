3dbde2732ec8d2f01ddc60aea362c026e5b78ac2
Rutuja <rutujapatil1306@gmail.com>
2024-10-09 13:21:41 +0530

    Added Education

--- src/main/java/com/gtasterix/AbhinavNGO/Config/ModelMapperConfig.java
+++ src/main/java/com/gtasterix/AbhinavNGO/Config/ModelMapperConfig.java
@@ -1,14 +1,14 @@
 package com.gtasterix.AbhinavNGO.Config;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ModelMapperConfig {
+public class ModelMapper {
 
-    @Bean
-    public ModelMapperConfig modelMapper(){
-        return new ModelMapperConfig();
-    }
+   @Bean
+    public ModelMapper modelMapper(){
+       return new ModelMapper();
+   }
+
 }
